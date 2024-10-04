4315e2fe141054f854b66c18b708854301d30022
Mateo-Sudo-24 <mateo.paredes@epn.edu.ec>
2024-10-04 11:59:14 +0200

    EJERCICIO 3 COMPLETO

--- src/org/example/Main.java
+++ src/org/example/Main.java
@@ -41,5 +41,7 @@ public class Main {
                 System.out.println(i);
             }
         }
+        System.out.println("###############");
+        System.out.println("Hola Mundo");
     }
 }
